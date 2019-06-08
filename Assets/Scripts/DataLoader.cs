fbf1a0417ddec806095ef000499f6f2d1e048e64
Elias Pujols <eliasnurbs@gmail.com>
2019-06-08 14:35:35 -0400

    GameObject to get the score data added to the GameOver Scene

---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour
+{
+    [SerializeField] GameSettings gameSettings;
+    [SerializeField] TextMeshProUGUI actualScoreText;
+    [SerializeField] TextMeshProUGUI bestScoreText;
+    [SerializeField] TextMeshProUGUI gameOverMessageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        actualScoreText.text = LoadActualScore();
+        gameOverMessageText.text = gameSettings.gameOverMessage;
+        bestScoreText.text = LoadBestScore();
+    }
+
+    private string LoadActualScore()
+    {
+        return $"Your score: { GameManager.gameScore.ToString() }";
+    }
+
+    private string LoadBestScore()
+    {
+        // TODO GET DATA FROM DATA BASE
+        return "Best score: 99999";
+    }
+}
